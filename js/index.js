f5c4dc5e4ad6311470ad76c503f0110c2a8e3fc2
gowtham-maarii <gowthammarisetti65@gmail.com>
2023-12-21 18:29:54 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,105 @@
+function redirectToTreatmentPage() {
+    window.location.href = 'video-page.html';
+}
+
+var videoplayer = [
+    {
+        video_thumbnail:"https://i.ytimg.com/vi/uYPbbksJxIg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2M_LSAVX5C1yeaypLoWxlz78y5Q",
+        logo_holder:"https://yt3.ggpht.com/ytc/AGIKgqP5NbFLeZgFjQ1d1WtzIrbenjlOjxLYTP0laCdo0Q=s68-c-k-c0x00ffffff-no-rj",
+        title_container:"\"OPPENHEIMER\" - New Trailer (Universal Pictures) - HD (Telugu)",
+        analytics_container:"Sony South Music"
+    },
+    {
+        video_thumbnail:"https://i.ytimg.com/vi/uYPbbksJxIg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2M_LSAVX5C1yeaypLoWxlz78y5Q",
+        logo_holder:"https://yt3.ggpht.com/ytc/AGIKgqP5NbFLeZgFjQ1d1WtzIrbenjlOjxLYTP0laCdo0Q=s68-c-k-c0x00ffffff-no-rj",
+        title_container:"\"OPPENHEIMER\" - New Trailer (Universal Pictures) - HD (Telugu)",
+        analytics_container:"Sony South Music"
+    },
+    {
+        video_thumbnail:"https://i.ytimg.com/vi/uYPbbksJxIg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2M_LSAVX5C1yeaypLoWxlz78y5Q",
+        logo_holder:"https://yt3.ggpht.com/ytc/AGIKgqP5NbFLeZgFjQ1d1WtzIrbenjlOjxLYTP0laCdo0Q=s68-c-k-c0x00ffffff-no-rj",
+        title_container:"\"OPPENHEIMER\" - New Trailer (Universal Pictures) - HD (Telugu)",
+        analytics_container:"Sony South Music"
+    },
+    {
+        video_thumbnail:"https://i.ytimg.com/vi/uYPbbksJxIg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2M_LSAVX5C1yeaypLoWxlz78y5Q",
+        logo_holder:"https://yt3.ggpht.com/ytc/AGIKgqP5NbFLeZgFjQ1d1WtzIrbenjlOjxLYTP0laCdo0Q=s68-c-k-c0x00ffffff-no-rj",
+        title_container:"\"OPPENHEIMER\" - New Trailer (Universal Pictures) - HD (Telugu)",
+        analytics_container:"Sony South Music"
+    },
+    {
+        video_thumbnail:"https://i.ytimg.com/vi/uYPbbksJxIg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2M_LSAVX5C1yeaypLoWxlz78y5Q",
+        logo_holder:"https://yt3.ggpht.com/ytc/AGIKgqP5NbFLeZgFjQ1d1WtzIrbenjlOjxLYTP0laCdo0Q=s68-c-k-c0x00ffffff-no-rj",
+        title_container:"\"OPPENHEIMER\" - New Trailer (Universal Pictures) - HD (Telugu)",
+        analytics_container:"Sony South Music"
+    },
+    {
+        video_thumbnail:"https://i.ytimg.com/vi/uYPbbksJxIg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2M_LSAVX5C1yeaypLoWxlz78y5Q",
+        logo_holder:"https://yt3.ggpht.com/ytc/AGIKgqP5NbFLeZgFjQ1d1WtzIrbenjlOjxLYTP0laCdo0Q=s68-c-k-c0x00ffffff-no-rj",
+        title_container:"\"OPPENHEIMER\" - New Trailer (Universal Pictures) - HD (Telugu)",
+        analytics_container:"Sony South Music"
+    },
+    {
+        video_thumbnail:"https://i.ytimg.com/vi/uYPbbksJxIg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2M_LSAVX5C1yeaypLoWxlz78y5Q",
+        logo_holder:"https://yt3.ggpht.com/ytc/AGIKgqP5NbFLeZgFjQ1d1WtzIrbenjlOjxLYTP0laCdo0Q=s68-c-k-c0x00ffffff-no-rj",
+        title_container:"\"OPPENHEIMER\" - New Trailer (Universal Pictures) - HD (Telugu)",
+        analytics_container:"Sony South Music"
+    },
+    {
+        video_thumbnail:"https://i.ytimg.com/vi/uYPbbksJxIg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2M_LSAVX5C1yeaypLoWxlz78y5Q",
+        logo_holder:"https://yt3.ggpht.com/ytc/AGIKgqP5NbFLeZgFjQ1d1WtzIrbenjlOjxLYTP0laCdo0Q=s68-c-k-c0x00ffffff-no-rj",
+        title_container:"\"OPPENHEIMER\" - New Trailer (Universal Pictures) - HD (Telugu)",
+        analytics_container:"Sony South Music"
+    },
+    {
+        video_thumbnail:"https://i.ytimg.com/vi/uYPbbksJxIg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2M_LSAVX5C1yeaypLoWxlz78y5Q",
+        logo_holder:"https://yt3.ggpht.com/ytc/AGIKgqP5NbFLeZgFjQ1d1WtzIrbenjlOjxLYTP0laCdo0Q=s68-c-k-c0x00ffffff-no-rj",
+        title_container:"\"OPPENHEIMER\" - New Trailer (Universal Pictures) - HD (Telugu)",
+        analytics_container:"Sony South Music"
+    },
+
+];
+
+
+rendercontent();
+
+function rendercontent()
+{
+    var content = document.getElementsByClassName('content');
+    console.log(content);                                   
+                                                                          
+    var content_card= content.querySelector('.content-card');
+    console.log(content_card);
+
+    let html= '';
+
+    videoplayer.forEach((d)=>{  
+    html+=`
+    <div class="video-thumbnail">
+    <img src="${d.video_thumbnail}" alt="Oppenheimer" />
+  </div>
+  <div class="video-desc">
+    <div class="logo-container">
+      <div class="logo-holder">
+        <img src="${d.logo_holder}"alt="logo">
+      </div>
+    </div>
+    <div class="info-container">
+      <div class="title-container">
+          <h4>${d.title_container}</h4>
+        <div class="menu-dots">
+
+
+        </div>
+      </div>
+      <div class="analytics-container">
+        <span>${d.analytics_container}</span> <br>
+        <span>12 M views </span>
+        <span> * 1 month ago</span>
+      </div>
+    </div>
+  </div>`
+
+  });
+  content_card.innerHTML=html; // 
+}
